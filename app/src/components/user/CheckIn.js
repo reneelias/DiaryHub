0278606b23205172c35cdb00f0ad0a6defb53cfb
darylortiz100 <dortiz4@mail.sfsu.edu>
2019-05-15 18:58:27 -0700

    Updated CheckIn.js

--- app/src/components/user/CheckIn.js
+++ app/src/components/user/CheckIn.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
 import styled from 'styled-components';
 import NavBar from '../main/NavBar'
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Table } from 'semantic-ui-react';
 import axios from 'axios'
 
 const Container = styled.div`
@@ -11,21 +11,32 @@ const Container = styled.div`
   margin-top: 30px;
   padding: 10px;
 `
-
-const Title = styled.h1`
-  text-align: center;
-  margin-bottom: 35px;
+const StyledLabel = styled.label`
+  font-weight: bold;
+  display: block;
 `
-
-const Style = {
-  marginBottom: '10px'
-}
+const Header = styled.div`
+  background-color: #24292e;
+  height: 40px;
+  color: white;
+  font-size: 20px;
+  padding-top: 9px;
+  padding-left: 15px;
+  border-radius: 6px 6px 0px 0px;
+  `
+  const Wrapper = styled.div`
+  max-width: 630px;
+  margin: 0 auto;
+  margin-top: 30px;
+  padding: 10px;
+`
 
 class CheckIn extends Component {
 
   state ={
     user_id: localStorage.getItem('user_id'),
     isAuth: localStorage.getItem('isAuth'),
+    user_details: [],
     weight: '',
     chest_width: '',
     waist_width: '',
@@ -52,6 +63,7 @@ class CheckIn extends Component {
   checkin = e => {
     e.preventDefault()
     const { user_id, weight, chest_width, waist_width, hip_width, mile_time, bench_weight } = this.state
+    
 
     if (weight !== 0 && chest_width !== 0 && waist_width !== 0 & hip_width !== 0) {
       axios.post('/checkin/add', {
@@ -78,11 +90,52 @@ class CheckIn extends Component {
           console.log(err.response.data)
         })
   } else {
-      alert('fill out fields')
+      alert('fill out mandatory fields')
+  }
+}
+
+changeWeight = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({weight: e.target.value})
+  }
+}
+
+changeChestWidth = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({chest_width: e.target.value})
+  }
+} 
+
+changeWaistWidth = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({waist_width: e.target.value})
+  }
+}
+
+changeHipWidth = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({hip_width: e.target.value})
+  }
+}
+changeMileTime = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({mile_time: e.target.value})
+  }
+}
+changeBenchWeight = e => {
+  const regex = /^[0-9\b]+$/;
+  if (e.target.value === '' || regex.test(e.target.value)) {
+    this.setState({bench_weight: e.target.value})
   }
 }
 
   render() {
+    const { user_details } = this.state
 
     if (this.state.isAuth !== 'true') {
       return <Redirect to="/" />
@@ -91,37 +144,91 @@ class CheckIn extends Component {
       <>
         <NavBar/>
         <Container>
-          <Title><big>Check In</big></Title>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Field>
-              <label style={Style}><big><h2>Weight*</h2></big></label>
-              <input style={Style} placeholder='Enter your weight'/>
-            </Form.Field>
-            <label style={Style}><h3>Width Measurement Inches</h3></label>
-            <Form.Field>
-              <label style={Style}><h4>Chest</h4></label>
-              <input style={Style} placeholder='Enter your chest width'/>
-            </Form.Field>
-            <Form.Field>
-              <label style={Style}><h4>Waist</h4></label>
-              <input style={Style} placeholder='Enter your waist width'/>
-            </Form.Field>
-            <Form.Field>
-              <label style={Style}><h4>Hip</h4></label>
-              <input style={Style} placeholder='Enter your hip width'/>
-            </Form.Field>
-            <Form.Field>
-              <label style={Style}><h3>Personal Goals (optional)</h3></label>
-              <label style={Style}><h4>Mile Time</h4></label>
-              <input style={Style} placeholder='Enter your mile time'/>
-            </Form.Field>
-            <Form.Field>
-              <label style={Style}><h4>Bench Press Personal Record</h4></label>
-              <input style={Style} placeholder='Enter maximum bench weight'/>
-            </Form.Field>
-            <p align= "right"><Button type='submit' color='black' size='big'>Record</Button></p>
+        <Header as='h2' color='black' textAlign='center'>CheckIn</Header>
+            <Form size='large' onSubmit={this.checkin}>
+            <StyledLabel>Weight*</StyledLabel>
+              <Form.Input 
+                fluid icon='weight' 
+                iconPosition='left' 
+                placeholder='Enter your weight (lbs)' 
+                value={this.state.weight}
+                onChange={(e) => {this.changeWeight(e)}}
+                maxLength="3"
+              />
+            <StyledLabel>Width Measurement Inches</StyledLabel>
+            <StyledLabel>Chest</StyledLabel>
+              <Form.Input 
+                fluid icon='male' 
+                iconPosition='left' 
+                placeholder='Enter your chest width' 
+                value={this.state.chest_width}
+                onChange={(e) => {this.changeChestWidth(e)}}
+                maxLength="3"
+              />
+            <StyledLabel>Waist</StyledLabel>
+              <Form.Input 
+                fluid icon='child' 
+                iconPosition='left' 
+                placeholder='Enter your waist width' 
+                value={this.state.waist_width}
+                onChange={(e) => {this.changeWaistWidth(e)}}
+                maxLength="9"
+              />
+              <StyledLabel>Hip</StyledLabel>
+              <Form.Input 
+                fluid icon='street view' 
+                iconPosition='left' 
+                placeholder='Enter your hip width' 
+                value={this.state.hip_width}
+                onChange={(e) => {this.changeHipWidth(e)}}
+                maxLength="9"
+              />
+            <StyledLabel>Mile Time</StyledLabel>
+              <Form.Input 
+                fluid icon='clock outline' 
+                iconPosition='left' 
+                placeholder='Enter your mile time' 
+                value={this.state.mile_time}
+                onChange={(e) => {this.changeMileTime(e)}}
+                maxLength="30"
+              />
+            <StyledLabel>Bench Press</StyledLabel>
+              <Form.Input 
+                fluid icon='anchor' 
+                iconPosition='left' 
+                placeholder='Enter your best bench weight (lbs)' 
+                value={this.state.bench_weight}
+                onChange={(e) => {this.changeBenchWeight(e)}}
+                maxLength="3"
+              />
+            <Button color='black' fluid size='large'>Submit</Button>
           </Form>
         </Container>
+        <Wrapper>
+      <h1 style={{textAlign: 'center'}}>Check In Summary</h1>
+      <Table color='black'>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Weight</Table.HeaderCell>
+            <Table.HeaderCell>Width Measurement</Table.HeaderCell>
+            <Table.HeaderCell>Waist</Table.HeaderCell>
+            <Table.HeaderCell>Hip</Table.HeaderCell>
+            <Table.HeaderCell>Mile Time</Table.HeaderCell>
+            <Table.HeaderCell>Bench Weight</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{user_details.weight}lbs</Table.Cell>
+            <Table.Cell>{user_details.chest_width}inches</Table.Cell>
+            <Table.Cell>{user_details.waist_width}inches</Table.Cell>
+            <Table.Cell>{user_details.hip_width}inches</Table.Cell>
+            <Table.Cell>{user_details.mile_time}</Table.Cell>
+            <Table.Cell>{user_details.bench_weight}lbs</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      </Wrapper>
       </>
     )
   }
